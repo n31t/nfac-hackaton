effd39d61cc4cab4c75f3516806d48a51d87bdac
Amir Adilov <amiradilov202@gmail.com>
2024-07-11 23:40:28 +0500

    n

--- src/vector-gpt/vector-gpt.service.ts
+++ src/vector-gpt/vector-gpt.service.ts
@@ -14,72 +14,68 @@ const indexName = "nfac-hackaton";
 const index = pinecone.index(indexName);
 
 class VectorGPTService {
-  async mapPointsToCategory(points: number): Promise<string> {
-    if (points < 20) {
-      return "hell-no";
-    } else if (points >= 20 && points < 40) {
-      return "no";
-    } else if (points >= 40 && points < 60) {
-      return "idk";
-    } else if (points >= 60 && points < 80) {
-      return "yes";
-    } else if (points >= 80) {
-      return "hell-yes";
-    } else {
-      return "Invalid points";
-    }
-  }
-
-  async createTotalMarks(
-    userJSONdata: UserData,
-    neededSkills: string
-  ): Promise<any> {
-    let points = 50;
-    if (
-      userJSONdata.availabilityInAlmaty === false ||
-      userJSONdata.gitHubHandle === "" ||
-      (userJSONdata.phoneNumber.length === 0 && userJSONdata.email === "") ||
-      userJSONdata.programmingExperienceDescription === "" ||
-      userJSONdata.pastProgrammingProjects === ""
-    ) {
-      points = 0;
-      let yesOrNo = "hell-no";
-      let opinionAboutParticipant = "Нет важных полей для проверки";
-      return { yesOrNo, points, opinionAboutParticipant };
+    async mapPointsToCategory(points: number): Promise<string> {
+        if (points < 20) {
+            return 'hell-no';
+        } else if (points >= 20 && points < 40) {
+            return 'no';
+        } else if (points >= 40 && points < 60) {
+            return 'idk';
+        } else if (points >= 60 && points < 80) {
+            return 'yes';
+        } else if (points >= 80) {
+            return 'hell-yes';
+        } else {
+            return 'Invalid points';
+        }
     }
 
-    const urlRegex = /^(http|https):\/\/[^ "]+$/;
-    if (urlRegex.test(userJSONdata.linkedInLink)) {
-      points += 2;
-    }
-    if (userJSONdata.cv) {
-      points += 5;
-    }
+    async createTotalMarks(userJSONdata: UserData, neededSkills : string) : Promise<any> { 
+        let points = 50;
+        if(userJSONdata.availabilityInAlmaty === false || 
+            userJSONdata.gitHubHandle === '' || 
+        (userJSONdata.phoneNumber.length === 0 && userJSONdata.email === '') || 
+        userJSONdata.programmingExperienceDescription === '' || 
+        userJSONdata.pastProgrammingProjects === ''){
+            points = 0;
+            let yesOrNo = "hell-no"
+            let opinionAboutParticipant = "Нет важных полей для проверки"
+            return { yesOrNo, points, opinionAboutParticipant };
+        }
+    
+        const urlRegex = /^(http|https):\/\/[^ "]+$/;
+        if(urlRegex.test(userJSONdata.linkedInLink)) {
+            points += 2;
+        }
+        if(userJSONdata.cv){
+            points += 5;
+        }
+        
+        // TODO: Check if user has projects in Github
+    
+    
+        const { fullName, email, birthDate, phoneNumber, willingToParticipateOnPaidBasis, 
+            telegramHandle, socialMediaLinks, 
+            // educationalPlacement, specialtyAtUniversity, jobPlacement, QUITE USELESS BUT MAYBE
+            availabilityInAlmaty, needAccommodationInAlmaty, gitHubHandle,  ...prompt } = userJSONdata;
+            console.log('Prompt:', prompt);
+            const embeddedPrompt = await new GoogleGenerativeAIEmbeddings().embedQuery(JSON.stringify(prompt));
+        
+            console.log("Embedded Prompt:", embeddedPrompt); 
+            let promptResponse = await index.query({
+              vector: embeddedPrompt,
+              topK: 1, // Retrieve more vectors initially
+              includeMetadata: true,
+            });
 
-    // TODO: Check if user has projects in Github
 
-    const {
-      fullName,
-      email,
-      birthDate,
-      phoneNumber,
-      willingToParticipateOnPaidBasis,
-      telegramHandle,
-      socialMediaLinks,
-      // educationalPlacement, specialtyAtUniversity, jobPlacement, QUITE USELESS BUT MAYBE
-      availabilityInAlmaty,
-      needAccommodationInAlmaty,
-      gitHubHandle,
-      ...prompt
-    } = userJSONdata;
-    console.log("Prompt:", prompt);
-    try {
-      const response = await openai.chat.completions.create({
-        model: "gpt-4o",
-        messages: [
-          {
-            role: "system",
-            content: `
+            try {
+            const response = await openai.chat.completions.create({
+                model: 'gpt-4o',
+                messages: [
+                    {
+                        role: 'system',
+                        content: `
                         Вы — автоматический проверяющий для летнего инкубатора nfactorial по программированию. Ваша задача — оценить кандидата по нескольким критериям и решить подходит ли он для дальнейшего прохождения. Вы смотрите критерии и ставите балл за этот критерий, относительно того, как он подходит. Учтите также, что большая часть полученных данных может получать больше и меньше баллов относительно запроса Ментора: "${neededSkills}".Ответ должен быть строго в формате JSON объекта и не должен включать никакого дополнительного текста. Критерии и максимальные баллы за них следующие:
                         
                         2. Опыт программирования относительно менторского запроса. От -30 до 20 баллов.
@@ -116,6 +112,9 @@ class VectorGPTService {
             content: `
                         Данные кандидата: {
                             ${JSON.stringify(prompt)}
+                        }
+                        Cхожие данные кандидата с комментарием от ментора: {
+                          ${JSON.stringify(promptResponse)}
                         }
                         `,
           },
@@ -156,6 +155,27 @@ class VectorGPTService {
       );
     }
   }
+
+  async saveToVectorDB(user: UserData, message: string) {
+    // Create a text string from the user's data and the message
+    const text = `${user} ${message}`;
+
+    // Generate an embedding for this text
+    const embedding = await embeddings.embedDocuments([text]);
+
+    // Flatten the embedding
+    const flattenedEmbedding = embedding.flat();
+
+    // Upsert the data to the Pinecone index
+    await index.upsert([{
+        id: user.email,
+        values: flattenedEmbedding,
+        metadata: {
+            ...user,
+            message
+        }
+    }]);
+}
 }
 
 export default VectorGPTService;
